833df1b20c301aa71431f2f7151fe65afbf28cf1
spajic031 <spajic_os@hotmail.com>
2023-12-13 20:55:08 +0100

    useRef

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
   return (
     <div className="flex">
       <Dropdown options={options} value={selected} onChange={handleSelect} />
-      <Dropdown options={options} value={selected} onChange={handleSelect} />
     </div>
   );
 }

fe93c04ef3fa22124a721a3f314327b3a509c7a2
spajic031 <spajic_os@hotmail.com>
2023-12-16 21:57:35 +0100

    1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import SideBar from "./components/SideBar";
+import AccordionPage from "./components/pages/AccordionPage";
+import ButtonPage from "./components/pages/ButtonPage";
+import DropdownPage from "./components/pages/DropdownPage";
 
 function App() {
   return (
-    <div>
-      <SideBar />
+    <div className="flex">
+      <div>
+        <SideBar />
+      </div>
+      <div>
+        <AccordionPage />
+        <ButtonPage />
+        <DropdownPage />
+      </div>
     </div>
   );
 }

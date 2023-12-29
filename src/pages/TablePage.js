c528aa7ff13b3f25d0b52e1ebc468f139d29c449
spajic031 <spajic_os@hotmail.com>
2023-12-29 12:59:32 +0100

    1

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -9,9 +9,22 @@ const TablePage = () => {
     { name: "lime", color: "bg-green-300", score: 4 },
   ];
 
+  const config = [
+    { label: "Name", render: (fruit) => fruit.name },
+    {
+      label: "Color",
+      render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`} />,
+    },
+    { label: "Score", render: (fruit) => fruit.score },
+  ];
+
+  const keyFn = (fruit) => {
+    return fruit.name;
+  };
+
   return (
     <div>
-      <Table data={data} />
+      <Table data={data} config={config} keyFn={keyFn} />
     </div>
   );
 };

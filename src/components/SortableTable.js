15ba29f88404525d51625ef8d910055b28fa4bcc
spajic031 <spajic_os@hotmail.com>
2024-01-02 20:45:42 +0100

    1

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -9,6 +9,12 @@ function SortableTable(props) {
   const [sortBy, setSortBy] = useState(null);
 
   const handleClick = (label) => {
+    if (sortBy && label !== sortBy) {
+      setSortOrder("asc");
+      setSortBy(label);
+      return;
+    }
+
     if (sortOrder === null) {
       setSortOrder("asc");
       setSortBy(label);
@@ -58,12 +64,7 @@ function SortableTable(props) {
     });
   }
 
-  return (
-    <div>
-      {sortOrder} - {sortBy}
-      <Table {...props} data={sortedData} config={updatedConfig} />
-    </div>
-  );
+  return <Table {...props} data={sortedData} config={updatedConfig} />;
 }
 
 function getIcons(label, sortBy, sortOrder) {
